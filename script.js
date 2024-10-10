d0a2af3eb95947bfcc11ad6e71044e1b5555254c
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-11 04:12:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let start = 0;
+let end = s2.length;
+
+if(s1.length < s2.length){
+	return -1;
+}else{
+	while(end <= s1.length){
+		if(s1.slice(start,end).toLowerCase() === s2.toLowerCase()){
+			
+			return start;
+			
+		}else{
+			start+=1;
+			end+=1;
+		}
+	}
+	return -1;
+}
 }
 
 // Please do not change the code below
